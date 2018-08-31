7b3f912a86fc17c4fc16f10430f2efe3b54793cf
gitgud <xdevzone@live.fr>
2018-08-31 02:04:02 +0200

    test

---
+++ ctf_mvps_rank.js
@@ -0,0 +1,149 @@
+// ------------------------------------------------------------------------
+//   Stay Alive for StarMash
+// ------------------------------------------------------------------------
+!function () {
+    /* INIT */
+    function init () {
+        console.log('init CTF_MVPs_Rank');
+        initEvents ();
+        
+    }
+
+    function initEvents () {
+        SWAM.on ( 'keydown', onKeydown );
+        // TODO on CTF match started, wait 10 sec and check if we are in spec, if so lauch idletime, and if stayalive = true, launch also the countdown
+        SWAM.on ( 'CTF_MatchStarted', onMatchStarted );
+    }
+    
+    SWAM.on ( 'gameLoaded', init );
+    
+    jQuery.fn.justtext = function() {
+  
+        return $(this)	.clone()
+                .children()
+                .remove()
+                .end()
+                .text();
+
+    };
+    
+    
+    /* GUI */
+    
+    $('body').append ("<div id='mvprankcontainer' style='display: none;'><div id='mvprank' style='display: block; position: absolute;left: 50%;margin: 0px 0px 0px -125px;bottom: 4px;width: 150px;height: 25px;padding: 5px;background: rgba(0, 247, 0, 0.5);border-radius: 5px;text-align: center;color: #EEE;font-size: 15px;cursor: pointer;'>Rank</div></div>");
+
+    $("#sf").click(function (){
+        console.log("Rank clicked");
+        
+        jQuery.fn.justtext = function() {
+  
+            return $(this)	.clone()
+                    .children()
+                    .remove()
+                    .end()
+                    .text();
+
+        };
+        
+        
+        parray = [];
+        var data = {};
+        $( "#scorecontainer .item" ).each(function( index ) {
+            var data = {};
+            console.log( index + ": " + $( this ).text() );
+            data.plyrid = $( this ).attr('player-id');
+            
+            data.plyrname = $( this ).children( ".name" ).children( ".player" ).text();
+            console.log( index + " plyrname: " + data.plyrname);
+            
+            pkills = $( this ).children( ".kills" ).text();
+            pdeaths = $( this ).children( ".deaths" ).text();
+            pcaps = $( this ).children( ".captures" ).text();
+            // gota ask statsbot
+            // precapskills = 
+            // precapsreturns = 
+            // pgamesplayed = 
+            
+            // avoid infinity ratio if deaths = 0
+            if (pdeaths == 0){
+                console.log( index + ": deaths count correction");
+                pdeaths = 1;
+            }
+            // correct caps count if caps = 0
+            if (pcaps == ' '){
+                console.log( index + ": caps count correction");
+                pcaps = 0;
+            }
+            
+            pkd = (pkills / pdeaths);
+            
+            // TODO : use bounty somewhere
+            // NOTE : probably use something like (bounty * k/d) to avoid meaningless bounty of kill-less players
+            data.pscore = (pcaps * 1000) + ((pcaps * 1000) * pkd) + (pkd * 100) ;
+            
+            console.log( index + " kd: " + pkd );
+            console.log( index + " score: " + data.pscore );
+            parray.push(data);
+        });
+        parray.sort(function(obj1, obj2) {
+            // Ascending:
+            return obj1.pscore - obj2.pscore;
+        }).reverse();
+        
+    });
+
+    
+    SWAM.on ( 'gamePrep', function (){
+        window.osf = config.scalingFactor;
+        console.log("gameprep osf =" + osf);
+        $("#sfcontainer").css({display: "none"});
+    });
+    
+    
+    
+    function onKeydown ( event ) {
+        
+        if ( event.originalEvent.key === 'v' ) { //note: This is not reliable to know if player is actually spectating
+
+            event.stopImmediatePropagation ();
+            
+            // game.spectatingID is not reliable, as it is null at first when spectating, until we spectate another player      
+            checkspecdelay = 2000;
+            checkspec(checkspecdelay)
+               
+            
+        }
+        
+            
+               
+            
+        }
+        
+    }
+    
+    function onMatchStarted () {
+        checkspecdelay = 10000;
+        checkspec(checkspecdelay)
+    }
+    
+    function checkspec(checkspecdelay){
+        window.setTimeout(function () {
+                    if( $('#btnFreeSpectator').css('display') == 'block' ) {
+                        console.log("v key pressed, show sf");
+                        $("#mvprankcontainer").css({display: "block"});
+                        
+                    }
+                },checkspecdelay); 
+    }
+
+    /* REGISTER */
+
+    SWAM.registerExtension ({
+        name: 'CTF_MVPs_Rank',
+        id: 'CTF_MVPs_Rank',
+        description: '',
+        version: '1.0.0',
+        author: 'xplay'
+    });
+    
+}();
